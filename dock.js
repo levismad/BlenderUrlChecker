ffce7c26b020b3d91f0ad8e1000e8f95a499d6fe
Levi Esteves <levi.junior@fcamara.com.br>
2019-04-28 14:05:56 -0300

    batch

--- dock.js
+++ dock.js
@@ -1,13 +1,23 @@
 var exec = require('child_process').exec;
 var urls = require('./config/urls.json').urls;
-
-// for(var i = 0; i < urls.length; i++){
-for(var i = 0; i < 100; i++){
-console.log(`Docker ${i}/${urls[i]}`)
-exec(`sudo docker run -t -i -e BARRAMENTO=${urls[i]} levismad/crawler`, function(error, stdout, stderr) {
-    if (error) {
-      console.log(error.code);
+var batchSize = 10;
+async function exe(){
+var proms = [];
+  // for(var i = 0; i < urls.length; i++){
+  for(var i = 0; i < batchSize + 1; i++){
+    if(i%100 == 0){
+      console.log(`aguardando`);
+      await Promise.all(proms);
+      console.log(`fim das ${i*batchSize} tasks`);
     }
-    console.log(stdout);
-  });
+    else{
+      console.log(`Docker ${i}/${urls[i]}`)
+      proms.push(exec(`sudo docker run -d -t -i -e BARRAMENTO=${urls[i]} levismad/crawler`, function(error, stdout, stderr) {
+        if (error) {
+          console.log(error.code);
+        }
+        console.log(stdout);
+      }));
+    }
+  }
 }
