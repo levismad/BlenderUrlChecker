c032c8041814a6a1bc8f5890c0afdc750bcc0972
Levi Esteves <levi.junior@fcamara.com.br>
2019-04-30 14:17:38 -0300

    relative paath

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ for i in {9000..159000..1000}
   do
         default=1000
         sum=$(($default + $i))
-        cross-env BARRAMENTO=$i,$sum node BlenderUrlChecker/index.js
+        cross-env BARRAMENTO=$i,$sum node index.js
  done
