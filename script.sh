1fb837dd1e23ee1625debf72efab344cb3772bd6
Levi Esteves <levi.junior@fcamara.com.br>
2019-04-30 14:15:09 -0300

    bash helper

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+for i in {9000..159000..1000}
+  do
+        default=1000
+        sum=$(($default + $i))
+        cross-env BARRAMENTO=$i,$sum node BlenderUrlChecker/index.js
+ done
