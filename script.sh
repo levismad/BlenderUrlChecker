6a2c028801dae45074b7e70c64189a5b507c8362
Levi Esteves <levi.junior@fcamara.com.br>
2019-05-01 16:40:45 -0300

    crazy

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@
 echo "Bash version ${BASH_VERSION}..."  
 echo "current: "$(env | grep CURRENT)
 currentStr=$(printenv CURRENT)
-current=$(($currentStr + 0))
+CURRENT=$(($currentStr + 0))
 currentStr=$(printenv TOTAL)
 total=$(($currentStr + 0))
 default=500
-for (( i=$current; i<=$total; i+= $default ))
+for (( i=$CURRENT; i<=$total; i+= $default ))
   do
-        echo "current: "$current
-        sum=$(($default + $i))
-        cross-env BARRAMENTO=$i,$sum node index.js
-        export CURRENT=$current
+        echo "current: "$CURRENT
+        CURRENT=$(($default + $i))
+        cross-env BARRAMENTO=$i,$CURRENT node index.js
+        export CURRENT
  done
